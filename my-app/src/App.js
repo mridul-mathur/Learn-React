9f0734275229f2271ffcd9b7abb5569c7aaf14e1
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-03-02 14:41:51 +0000

    $β

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,19 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I LOVE YOU
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" target="_blank" rel="noopener noreferrer">
+          SAKSHI
         </a>
       </header>
     </div>
   );
 }
-
 export default App;
