5e5bf7e03080eaf4cbe59dd397cb74e1c3302c34
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-03-12 06:28:21 +0000

    q

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,34 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My name is Mridul and I'm going to master React!
-        </p>
-        <a className="App-link" target="_blank" rel="noopener noreferrer">
-          isthatsb
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name:{firstname:'Sakshi', lastname:'Beniwal'},
+      occupation : 'Graphic Designer'
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name.firstname} {this.state.name.lastname} I am {this.state.occupation}</p>
+          <button 
+            onClick={() => {
+              this.setState({name:{firstname:'Mridul',lastname:'Mathur'}, occupation:'Web Developer'});
+              console.log(this.state);
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }}
 export default App;
+//it allows us to tell react what we expect this component to render and how we expect it to render.
