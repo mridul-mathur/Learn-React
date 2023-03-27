de5c5d3dccf7cf3df95afd07e4417e5ee4feae15
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-03-27 18:07:41 +0000

    .

--- my-app/src/array.js
+++ my-app/src/array.js
@@ -1,3 +1,8 @@
 const arr =[1,2,3,4];
 console.log(arr.map(el=> el +1)); // [2,4,6,8]
 console.log(arr); // [1,2,3,4]
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+const element = <h1>Hello, world</h1>;
+root.render(element);//render is a function that takes in a react element and renders it to the DOM.
