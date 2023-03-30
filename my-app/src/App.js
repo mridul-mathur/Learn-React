0ff2c44cbee8e9ea6430638acce848126bdb560c
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-03-30 06:01:46 +0000

    n

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Component } from 'react';
 import './App.css';
 
@@ -10,27 +11,21 @@ class App extends Component {
   }
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())//this is a promise also we are getting the response
-                                      // from the api, then we are converting it into json.
-    .then(users => this.setState({players:users}));//then we are setting the state of the
-                                                   // players to the users
-  }
+    .then(response => response.json())//this is a promise also we are getting the response from the api, then we are converting it into json.
+    .then(users => this.setState({players:users}));//then we are setting the state of the players to the users
+}
   render(){
     return(
       <div className="App"> 
         {this.state.players.map((player) => {
-            return <div key={player.id}><h1>{player.name}</h1></div>;//when ever we use map we
-                                                                     // need to have a key to 
-                                                                     //highest element i.e. Div
-                                                                     // here
-          })
+            return <div key={player.id}><h1>{player.name}</h1></div>;}) 
+            //when ever we use map we need to have a key to highest element i.e. Div here
         }
       </div>
     );
   }}
 export default App;
-//it allows us to tell react what we expect this component to render and how we expect it to
-//render.
+//it allows us to tell react what we expect this component to render and how we expect it to render.
 /*[ { name: 'Steph Curry',
       id : '12d2j431'
     },
